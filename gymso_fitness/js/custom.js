b3e3cf9d7bd698fef9338eb532daf36af6ee181a
munsaf22 <munsafhask7643@gmail.com>
2023-11-26 17:36:24 +0530

    new changes still not done

--- gymso_fitness/js/custom.js
+++ gymso_fitness/js/custom.js
@@ -28,4 +28,42 @@
 
 
     
+document.addEventListener("DOMContentLoaded", function() {
+    var currentIndex = 0;
+    function changeImage() {
+        const imageElement = document.querySelector("#main-img"); 
+        const imageSources = [
+            "images/testmonial/2.jpg",
+            "images/testmonial/3.jpg",
+            "images/testmonial/4.jpg",
+            "images/testmonial/5.jpg",
+            "images/testmonial/6.jpg",
+            "images/testmonial/7.jpg",
+            "images/testmonial/8.jpg",
+            "images/testmonial/9.jpg",
+            "images/testmonial/10.jpg",
+            "images/testmonial/11.jpg",
+            "images/testmonial/12.jpg",
+            "images/testmonial/13.jpg",
+            "images/testmonial/14.jpg",
+            "images/testmonial/15.jpg",
+            "images/testmonial/16.jpg",
+            "images/testmonial/17.jpg",
+            "images/testmonial/18.jpg",
+            "images/testmonial/19.jpg",
+        ];
+    if(currentIndex >= imageSources.length){
+        currentIndex=0;
+    }
+    //  console.log("hello",currentIndex);
+      imageElement.src = imageSources[currentIndex];
+      currentIndex ++;
+      setTimeout(changeImage, 3000);
+    }
+    changeImage();
+  });
 
+  function formOpen(){
+    $('#membershipForm').modal();
+    
+  }
